cc30249afa6d4c8bc7590c737fa94a3e2493791f
Mario Rogic <hello@mario.net.au>
2016-04-10 17:39:35 +0200

    Remove x-frame-options as it hasn't been useful / gets in the way

--- app.rb
+++ app.rb
@@ -12,18 +12,6 @@ get '/' do
   redirect '/index.html'
 end
 
-# True if x-frame-options are set, false otherwise
-get '/x-frame-options' do
-  return "true" if params['url'].empty?
-
-  begin
-    # TODO: does not cover (unlikely) edge case that xrespond is in allow-from
-    (!HTTParty.head(params['url']).headers['x-frame-options']).to_json
-  rescue
-    'maybe'.to_json
-  end
-end
-
 not_found do
   halt 404, 'Page not found'
 end
